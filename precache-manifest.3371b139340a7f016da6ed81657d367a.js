bee2547c8211932012048c6343382e3b786b4243
Kamran92 <k.r.a.92mail.ru>
2020-02-09 16:43:30 +0300

    Updates

---
+++ precache-manifest.3371b139340a7f016da6ed81657d367a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728f073015caae2bd543f818f1778463",
+    "url": "/organiser/index.html"
+  },
+  {
+    "revision": "f34fdc964b4b189623a7",
+    "url": "/organiser/static/css/main.b116d129.chunk.css"
+  },
+  {
+    "revision": "b6cc9a2639ba9874cc60",
+    "url": "/organiser/static/js/2.67667578.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/organiser/static/js/2.67667578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34fdc964b4b189623a7",
+    "url": "/organiser/static/js/main.ddc92249.chunk.js"
+  },
+  {
+    "revision": "638f90f60d1407b0b99c",
+    "url": "/organiser/static/js/runtime-main.7711f7c2.js"
+  }
+]);
